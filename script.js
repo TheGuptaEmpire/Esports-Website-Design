8865caf2746fb42e13f2c941b08e1e42128164ec
SG <shoury24gupta@gmail.com>
2022-08-10 19:13:57 +0530

    Navbar completed

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Esports Official Website');
+
+document.querySelector('#menu-btn').addEventListener('click', ()=> {
+    let headings1 = document.querySelector('.headings-1');
+    headings1.classList.toggle('active');
+})
